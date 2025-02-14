7e01dc08d436f8e99bc95880a8e97b1d847e4473
ankit satyal <ankitsatyal6@gmail.com>
2025-02-14 23:52:25 +0530

    text animation done

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+function breakTheText(){
+    let name = document.querySelector(".name");
+    let nameText = name.textContent;
+
+    let splitText = nameText.split("");
+    
+    let subtext = "";
+
+    splitText.forEach((letter)=>{
+        if(letter==="S"){
+            subtext +=`<span class="inline-block ml-5">S</span>`;
+        }else{
+            subtext += `<span class="inline-block">${letter}</span>`;
+        }
+    })
+
+    name.innerHTML = subtext;
+
+    console.log(subtext)
+
+    console.log(name)
+     
+}
+
+breakTheText();
+
+gsap.from(".name span",{
+    y: 40,
+    opacity:0,
+    duration : 1,
+    stagger : 0.1,
+})
+
+
 gsap.to(".user",{
     color : "white",
-    duration : 2,
+    duration : 1,
     opacity : 1,
     scrollTrigger : {
         trigger : ".intro-section",
@@ -30,14 +64,27 @@ gsap.to(".connect",{
 
 gsap.to(".middle-section",{
     y:-150,
-    opacity : 1,
     duration: 2,
     scrollTrigger : {
         trigger : ".middle-section",
         scroller : "body",
         // markers  : true,
-        start : "top 80%",
+        start : "top 50%",
         end :"top 70%",
+        scrub : 1,
+
+    }
+})
+
+gsap.to(".connect-section",{
+    y:-150,
+    duration: 2,
+    scrollTrigger : {
+        trigger : ".connect-section",
+        scroller : "body",
+        // markers  : true,
+        start : "top 100%",
+        end :"bottom 0%",
         scrub : 1,
 
     }
