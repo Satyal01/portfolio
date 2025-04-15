78fe54f7920b292fff22deff09c2bce7b92de5a6
ankit satyal <ankitsatyal6@gmail.com>
2025-04-16 00:48:11 +0530

    some improvments in mobile view

--- index.js
+++ index.js
@@ -1,125 +1,126 @@
-function breakTheText(){
-    let name = document.querySelector(".name");
-    let nameText = name.textContent;
+// the name animation
+function breakTheText() {
+  let name = document.querySelector(".name");
+  let nameText = name.textContent;
 
-    let splitText = nameText.split("");
-    
-    let subtext = "";
+  let splitText = nameText.split("");
 
-    splitText.forEach((letter)=>{
-        if(letter==="S"){
-            subtext +=`<span class="inline-block ml-5">S</span>`;
-        }else{
-            subtext += `<span class="inline-block">${letter}</span>`;
-        }
-    })
+  let subtext = "";
 
-    name.innerHTML = subtext;
+  splitText.forEach((letter) => {
+    if (letter === "S") {
+      subtext += `<span class="inline-block ml-5">S</span>`;
+    } else {
+      subtext += `<span class="inline-block">${letter}</span>`;
+    }
+  });
 
+  name.innerHTML = subtext;
 }
 
 breakTheText();
 
-gsap.from(".name span",{
-    y: 40,
-    opacity:0,
-    duration : 1,
-    stagger : 0.1
+gsap.from(".name span", {
+  y: 40,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.1,
+});
+
+
+// the scroll animation 
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 768px)",()=>{
+    gsap.to(".user", {
+        color: "white",
+        duration: 1,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".intro-section",
+          scroler: "body",
+          start: "top 30%",
+          end: "top 10%",
+          // markers : true,
+          toggleActions: "play reset play reset",
+        },
+      });
+      
+      gsap.to(".skills", {
+        color: "white",
+        duration: 2,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".skills-section",
+          toggleActions: "play reset play reset",
+        },
+      });
+      
+      gsap.to(".connect", {
+        color: "white",
+        duration: 5,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".connect-section",
+          scroller: "body",
+          start: "top 50%",
+          end: "bottom 40%",
+          // markers : true,
+          toggleActions: "play reset play reset",
+        },
+      });
+      
+      gsap.to(".middle-section", {
+        y: -150,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".middle-section",
+          scroller: "body",
+          // markers  : true,
+          start: "top 50%",
+          end: "top 70%",
+          scrub: 1,
+        },
+      });
+      
+      // gsap.to(".btech",{
+      //     y : 100,
+      //     duration :1,
+      //     scrollTrigger :{
+      //         trigger : ".middle-section",
+      //         scroller : "body",
+      //         start : "top 60%",
+      //         end : "top 70%",
+      //         j
+      //     }
+      // })
+      
+      gsap.to(".connect-section", {
+        y: -100,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".connect-section",
+          scroller: "body",
+          // markers  : true,
+          start: "top 50%",
+          end: "bottom 80%",
+          scrub: 1,
+        },
+      });
+      
+      
 })
 
 
-gsap.to(".user",{
-    color : "white",
-    duration : 1,
-    opacity : 1,
-    scrollTrigger : {
-        trigger : ".intro-section",
-        scroler : "body",
-        start : "top 30%",
-        end : "top 10%",
-        // markers : true,
-        toggleActions : "play reset play reset",
-    }
-})
-
-gsap.to(".skills",{
-    color : "white",
-    duration : 2,
-    opacity : 1,
-    scrollTrigger : {
-        trigger : ".skills-section",
-        toggleActions : "play reset play reset",
-    }
-})
-
-gsap.to(".connect",{
-    color : "white",
-    duration : 5,
-    opacity : 1,
-    scrollTrigger : {
-        trigger : ".connect-section",
-        scroller : "body",
-        start : "top 50%",
-        end : "bottom 40%",
-        // markers : true,
-        toggleActions : "play reset play reset",
-    }
-})
-
-gsap.to(".middle-section",{
-    y:-150,
-    duration: 1,
-    scrollTrigger : {
-        trigger : ".middle-section",
-        scroller : "body",
-        // markers  : true,
-        start : "top 50%",
-        end :"top 70%",
-        scrub : 1,
-
-    }
-})
-
-// gsap.to(".btech",{
-//     y : 100,
-//     duration :1,
-//     scrollTrigger :{
-//         trigger : ".middle-section",
-//         scroller : "body",
-//         start : "top 60%",
-//         end : "top 70%",
-//         j
-//     }
-// })
-
-
-
-
-gsap.to(".connect-section",{
-    y:-100,
-    duration:1,
-    scrollTrigger : {
-        trigger : ".connect-section",
-        scroller : "body",
-        // markers  : true,
-        start : "top 50%",
-        end :"bottom 80%",
-        scrub : 1,
-
-    }
-})
 
 // mail to  whitespace begin replace by + solution
-const email = document.querySelector(".form")
+const email = document.querySelector(".form");
 
-console.log(email);
+email.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const subject = encodeURIComponent(this.subject.value);
+  const body = encodeURIComponent(this.body.value);
 
-email.addEventListener("submit",function(e){
-    e.preventDefault();
-    console.log(this);
-    const subject = encodeURIComponent(this.subject.value);
-    const body= encodeURIComponent(this.body.value);
-
-    const mailtoLink = `mailto:ankitsatyal6@gmail.com?subject=${subject}&body=${body}`;
-    window.location.href = mailtoLink;
+  const mailtoLink = `mailto:ankitsatyal6@gmail.com?subject=${subject}&body=${body}`;
+  window.location.href = mailtoLink;
 });
